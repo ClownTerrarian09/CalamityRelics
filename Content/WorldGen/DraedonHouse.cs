3a261adb48e499d832fe6fa40bf91dc1addb2265
StallPlayz <stallplayz123@gmail.com>
2026-08-30 17:13:52 +0700

    Finished Draedon's House WorldGen

---
+++ Content/WorldGen/DraedonHouse.cs
@@ -0,0 +1,242 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+using Terraria;
+using Terraria.IO;
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using CalamityMod.Schematics;
+using Terraria.GameContent.Generation;
+
+namespace CalamityRelics.WorldGen
+{
+    public class RelicsIceStructureGen : ModSystem
+    {
+        private const string DraedonHouseSchematicKey = "CalamityRelics:DraedonHouse";
+
+        private const string DraedonHouseSchematicPath = "Content/Structures/DraedonHouse.csch";
+        private const string CalamityModName = "CalamityMod";
+        private const string CalamitySchematicIOType = "CalamityMod.Schematics.CalamitySchematicIO";
+        private const string CalamitySchematicManagerType = "CalamityMod.Schematics.SchematicManager";
+
+        private static HashSet<int> RestrictedTiles = new HashSet<int>();
+        private static int ElumplateID = -1;
+
+        public override void PostSetupContent()
+        {
+            RestrictedTiles.Clear();
+
+            RestrictedTiles.Add(TileID.LihzahrdBrick);
+            RestrictedTiles.Add(TileID.BlueDungeonBrick);
+            RestrictedTiles.Add(TileID.GreenDungeonBrick);
+            RestrictedTiles.Add(TileID.PinkDungeonBrick);
+
+            if (ModContent.TryFind("CalamityMod", "Elumplate", out ModTile elumplate))
+            {
+                ElumplateID = elumplate.Type;
+                RestrictedTiles.Add(ElumplateID);
+            }
+
+            string[] calamityLabTiles = new string[]
+            {
+                "LaboratoryPlating",
+                "LaboratoryPanels",
+                "HazardChevronPanels",
+                "LaboratoryPipePlating",
+                "LaboratoryPlateBeam",
+                "LaboratoryPlatePillar",
+                "RustedPlating",
+                "RustedPipes",
+                "RustedPlateBeam",
+                "RustedPlatePillar",
+                "Navyplate",
+                "Plagueplate",
+                "Cinderplate",
+                "Chaosplate"
+            };
+
+            foreach (string tileName in calamityLabTiles)
+            {
+                if (ModContent.TryFind("CalamityMod", tileName, out ModTile tile))
+                {
+                    RestrictedTiles.Add(tile.Type);
+                }
+            }
+
+            if (ModLoader.TryGetMod(CalamityModName, out Mod calamity))
+            {
+                try
+                {
+                    using (Stream stream = Mod.GetFileStream(DraedonHouseSchematicPath))
+                    {
+                        Type ioType = calamity.Code.GetType(CalamitySchematicIOType);
+                        MethodInfo importMethod = ioType.GetMethod("ImportSchematic", BindingFlags.NonPublic | BindingFlags.Static);
+                        object parsedSchematic = importMethod.Invoke(null, new object[] { stream });
+
+                        Type managerType = calamity.Code.GetType(CalamitySchematicManagerType);
+                        FieldInfo tileMapsField = managerType.GetField("TileMaps", BindingFlags.NonPublic | BindingFlags.Static);
+                        var tileMaps = (System.Collections.IDictionary)tileMapsField.GetValue(null);
+
+                        tileMaps[DraedonHouseSchematicKey] = parsedSchematic;
+                        Mod.Logger.Info($"Successfully injected {DraedonHouseSchematicPath} into Calamity's SchematicManager.");
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    Mod.Logger.Error($"Calamity Relics: Failed to inject schematic via reflection. {ex}");
+                }
+            }
+        }
+
+        public override void Unload()
+        {
+            RestrictedTiles?.Clear();
+            RestrictedTiles = null;
+            ElumplateID = -1;
+        }
+
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight)
+        {
+            int microBiomeIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Draedon Structures"));
+
+            if (microBiomeIndex != -1)
+            {
+                tasks.Insert(microBiomeIndex + 1, new PassLegacy("Draedon's House", (progress, configuration) =>
+                {
+                    progress.Message = "Forging Draedon's Past";
+                    PlaceIceStructure();
+                }));
+            }
+            else
+            {
+                Mod.Logger.Warn("Calamity Relics: Could not find 'Draedon Structures' generation pass. The Relics Ice Structure will not be generated.");
+            }
+        }
+
+        private void PlaceIceStructure()
+        {
+            bool placed = false;
+            int attempts = 0;
+            int maxAttempts = 10000;
+
+            Point? iceLabPosition = FindIceLabPosition();
+
+            while (!placed && attempts < maxAttempts)
+            {
+                attempts++;
+
+                int x = Main.rand.Next(200, Main.maxTilesX - 200);
+                int y = Main.rand.Next((int)Main.rockLayer, Main.maxTilesY - 300);
+
+                Tile tile = Main.tile[x, y];
+
+                if (tile.HasTile && (tile.TileType == TileID.IceBlock || tile.TileType == TileID.SnowBlock))
+                {
+                    if (iceLabPosition.HasValue)
+                    {
+                        float distanceFromLab = Vector2.Distance(new Vector2(x, y), new Vector2(iceLabPosition.Value.X, iceLabPosition.Value.Y));
+                        if (distanceFromLab < 300f)
+                        {
+                            continue;
+                        }
+                    }
+
+                    if (CheckIceBiomeDensity(x, y, 50, 400))
+                    {
+                        if (IsAreaClear(x, y, 60, 60))
+                        {
+                            bool specialCondition = false;
+
+                            SchematicManager.PlaceSchematic<System.Action<Terraria.Chest>>
+                            (
+                                DraedonHouseSchematicKey,
+                                new Point(x, y),
+                                SchematicAnchor.TopLeft,
+                                ref specialCondition,
+                                null
+                             );
+
+                            placed = true;
+                        }
+                    }
+                }
+            }
+
+            if (!placed)
+            {
+                Mod.Logger.Warn($"Calamity Relics: Failed to find a suitable location for {DraedonHouseSchematicPath}.");
+            }
+        }
+
+        private bool CheckIceBiomeDensity(int centerX, int centerY, int radius, int requiredTiles)
+        {
+            int iceCount = 0;
+
+            for (int i = centerX - radius; i <= centerX + radius; i++)
+            {
+                for (int j = centerY - radius; j <= centerY + radius; j++)
+                {
+                    if (i < 0 || i >= Main.maxTilesX || j < 0 || j >= Main.maxTilesY) continue;
+
+                    Tile tile = Main.tile[i, j];
+
+                    if (tile.HasTile)
+                    {
+                        if (tile.TileType == TileID.IceBlock ||
+                            tile.TileType == TileID.SnowBlock ||
+                            tile.TileType == TileID.CorruptIce ||
+                            tile.TileType == TileID.FleshIce ||
+                            tile.TileType == TileID.HallowedIce)
+                        {
+                            iceCount++;
+
+                            if (iceCount >= requiredTiles)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private Point? FindIceLabPosition()
+        {
+            if (ElumplateID == -1) return null;
+
+            for (int x = 100; x < Main.maxTilesX - 100; x += 15)
+            {
+                for (int y = (int)Main.rockLayer; y < Main.maxTilesY - 200; y += 15)
+                {
+                    Tile tile = Main.tile[x, y];
+                    if (tile.HasTile && tile.TileType == ElumplateID)
+                    {
+                        return new Point(x, y);
+                    }
+                }
+            }
+            return null;
+        }
+
+        private bool IsAreaClear(int startX, int startY, int width, int height)
+        {
+            for (int i = startX; i < startX + width; i++)
+            {
+                for (int j = startY; j < startY + height; j++)
+                {
+                    Tile checkTile = Main.tile[i, j];
+
+                    if (checkTile.HasTile && RestrictedTiles.Contains(checkTile.TileType))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
