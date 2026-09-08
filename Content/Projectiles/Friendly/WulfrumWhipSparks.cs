5a731fe0cd707a332f967dcafa6b51904a50d764
AquaDummy <116375234+AquaDummy@users.noreply.github.com>
2026-09-08 16:01:49 -0400

    Wulfrum Whiplash
    The wulfrum whip with the sprite. Should be all done.

--- Content/Projectiles/Friendly/WulfrumWhipSparks.cs
+++ Content/Projectiles/Friendly/WulfrumWhipSparks.cs
@@ -30,7 +30,7 @@ namespace CalamityRelics.Content.Projectiles.Friendly
             {
                 Particle spark2 = new BoltParticle(Projectile.Center + new Vector2(Main.rand.NextFloat(-8f, 8f), Main.rand.NextFloat(-8f, 8f)), 
             -Projectile.velocity + new Vector2(Main.rand.NextFloat(-1f, 1f), Main.rand.NextFloat(-1f, 1f)), false, 18, 0.15f, 
-            new Color(0, 216, 255, 0.1f), new Vector2(2.5f, 0.8f), true, true, false, 0.3f);
+            new Color(0, 216, 255, 0.08f), new Vector2(2.5f, 0.8f), true, true, false, 0.3f);
             GeneralParticleHandler.SpawnParticle(spark2);
             }
             
